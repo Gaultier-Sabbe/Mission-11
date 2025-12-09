b2b36924b60c7be2695a36d4475c96711f849e98
Alex <166955370+Naylome@users.noreply.github.com>
2025-12-09 23:12:56 +0100

    Mission 2.2 et 2.3

--- ListeLecture.py
+++ ListeLecture.py
@@ -67,3 +67,62 @@ class ListeLecture:
             i += 1
             cur = cur.next
         return s[:-1]  # return s sans le dernier "\n"
+
+
+from Duree import Duree
+from LinkedList import LinkedList
+
+class ListeLecture:
+
+    def __init__(self, name, id):
+        self.name = name
+        self.id = id
+        self.duree = Duree(0, 0, 0)
+        self.__medias = LinkedList()
+
+    def ajouter(self, media):
+
+        self.__medias.add(media)
+        self.duree.ajouter(media.duree)
+
+    def retirer(self, media):
+        if self.__medias.first() != None:
+            head = self.__medias.first()
+
+            if head.value() == media:
+                self.__medias.set_first(head.next())
+                self.__medias.dec_size()
+                if self.__medias.size() == 0:
+                    self.__medias.last = None
+                return
+
+            cur = head
+            while cur.next() is not None and cur.next().value() != media:
+                cur = cur.next()
+
+            if cur.next() is not None and cur.next().value() == media:
+                to_remove = cur.next()
+                cur.set_next(to_remove.next())
+                self.__medias.dec_size()
+
+                if cur.next() is None:
+                    self.__medias.last = cur
+
+    def nombre_de_medias(self):
+        return self.__medias.size()
+
+    def __str__(self):
+        
+        s = "[#{}] {} ({} medias)\n".format(self.id, self.name, self.nombre_de_medias())
+        i = 0
+        current = self.__medias.first()
+
+        while current is not None:
+            s += "{:02}: ".format(i) + str(current.value()) + "\n"
+            current = current.next()
+            i += 1
+
+        if i > 0:
+            return s[:-1]
+        return s
+
--- OrderedLinkedList.py
+++ OrderedLinkedList.py
@@ -1,52 +1,67 @@
-###########################
-# OrderedLinkedList class #
-###########################
+from LinkedList import LinkedList, Node
 
-class OrderedLinkedList:
-    class Node:
-        def __init__(self, data, pre=None, next=None):
-            self.data = data
-            self.pre = pre
-            self.next = next
+class OrderedLinkedList(LinkedList):
+
     def __init__(self, lst=[]):
         """
-        Initialises a new linked list object, with a given list of elements lst.
+        Initialises a new ordered linked list object, with a given list of elements lst.
         @pre:  -
         @post: A new ordered linked list object has been initialised.
                Its length is equal to the number of elements in lst.
-               The data elements stored in the linked list's nodes correspond to those of the list lst,
-               and appear in the same order as in that list.
-               If no list lst is passed as argument, the newly created linked list
-               will have 0 length, contain no nodes and its head will point to None. 
+               The data elements stored in the linked list's nodes correspondent to those of the list lst,
+               mais ils sont insérés de façon à respecter l'ordre défini par les comparaisons (<).
+               Si aucune liste lst n'est donnée, la liste est vide.
         """
-        pass
-        self.__head = None
-        self.__tail = None
-        self.__length = 0
-            
+        super().__init__([])
+        for elem in lst:
+            self.add(elem)
+
     def size(self):
         """
         Accessor method which returns the number of nodes contained in this ordered linked list.
         @pre:  -
         @post: Returns the number of nodes (possibly zero) contained in this ordered linked list.
         """
-        return self.__length
-        
+        return self.length
+
     def add(self, cargo):
-        """ 
+        """
         Adds a new Node with given cargo to the ordered linked list such that the order is maintained
-        @pre:  self is a (possibly empty) LinkedList.
+        @pre:  self is a (possibly empty) OrderedLinkedList.
         @post: A new Node object is created with the given cargo.
                This new Node is added to the ordered linked list in an ordered manner.
                The length counter has been incremented by 1.
         """
-        new_node = self.Node(cargo,self.__tail,None)
-        self.__tail.next = new_node
-        self.__tail = new_node
-        if self.__head == None:
-            self.__head = self.__tail
-        self.__length += 1
+        new_node = Node(cargo)
 
+        if self.first() is None:
+            self.set_first(new_node)
+            self.last = new_node
+            self.inc_size()
+            return
+
+        first = self.first()
+
+        if cargo <= first.value():
+            new_node.set_next(first)
+            self.set_first(new_node)
+            self.inc_size()
+            return
+
+        prev = first
+        cur = first.next()
+
+        while cur is not None and cur.value() <= cargo:
+            prev = cur
+            cur = cur.next()
+
+        new_node.set_next(cur)
+        prev.set_next(new_node)
+
+        if cur is None:
+            self.last = new_node
+
+        self.inc_size()
 
     def remove(self, cargo):
         """
@@ -54,21 +69,27 @@ class OrderedLinkedList:
         Leaves the list intact if already empty or if a node with that cargo
         doesn't exist.
         """
-        if self.__head != None:
-            done = False
-            if self.__head.data == cargo:
-                self.__head == self.__head.next
-                self.__head.pre = None
-                done = True
-            else:
-                cur = self.__head
-                while cur.next is not None and cur.next.data != cargo:
-                    if cur.next.data == cargo:
-                        cur.next = cur.next.next
-                        cur.next.pre = cur
-                        done = True
-                    cur = cur.next
-            if done:
-                self.__length -= 1
+        if self.first() != None:
+
+            head = self.first()
 
 
+            if head.value() == cargo:
+                self.set_first(head.next())
+                self.dec_size()
+                if self.size() == 0:
+                    self.last = None
+                return
+
+            prev = head
+            cur = head.next()
+
+            while cur is not None and cur.value() != cargo:
+                prev = cur
+                cur = cur.next()
+
+            if cur is not None:
+                prev.set_next(cur.next())
+                self.dec_size()
+                if cur.next() is None:
+                    self.last = prev
