12d20ed79f4cbab717bdf34490adfdbe326b786a
Gaultier-Sabbe <gaultier.sabbe@student.uclouvain.be>
2025-12-10 12:55:34 +0100

    listelecture + ordonnee

--- ListeLecture.py
+++ ListeLecture.py
@@ -1,28 +1,29 @@
 from Duree import Duree
+from LinkedList import LinkedList
 from OrderedLinkedList import OrderedLinkedList
 
-class ListeLecture(OrderedLinkedList):
+class ListeLecture:
 
     def __init__(self, name, id):
         self.name = name
         self.id = id
         self.duree = Duree(0, 0, 0)
-        self.__medias = OrderedLinkedList()
+        self.medias = LinkedList()
 
     def ajouter(self, media):
 
-        self.__medias.add(media)
+        self.medias.add(media)
         self.duree.ajouter(media.duree)
 
     def retirer(self, media):
-        if self.__medias.first() != None:
-            head = self.__medias.first()
+        if self.medias.first() != None:
+            head = self.medias.first()
 
             if head.value() == media:
-                self.__medias.set_first(head.next())
-                self.__medias.dec_size()
-                if self.__medias.size() == 0:
-                    self.__medias.last = None
+                self.medias.set_first(head.next())
+                self.medias.dec_size()
+                if self.medias.size() == 0:
+                    self.medias.last = None
                 return
 
             cur = head
@@ -32,19 +33,19 @@ class ListeLecture(OrderedLinkedList):
             if cur.next() is not None and cur.next().value() == media:
                 to_remove = cur.next()
                 cur.set_next(to_remove.next())
-                self.__medias.dec_size()
+                self.medias.dec_size()
 
                 if cur.next() is None:
-                    self.__medias.last = cur
+                    self.medias.last = cur
 
     def nombre_de_medias(self):
-        return self.__medias.size()
+        return self.medias.size()
 
     def __str__(self):
         
         s = "[#{}] {} ({} medias)\n".format(self.id, self.name, self.nombre_de_medias())
         i = 0
-        current = self.__medias.first()
+        current = self.medias.first()
 
         while current is not None:
             s += "{:02}: ".format(i) + str(current.value()) + "\n"
@@ -55,3 +56,8 @@ class ListeLecture(OrderedLinkedList):
             return s[:-1]
         return s
 
+class ListeLectureOrdonnee(ListeLecture):
+    def __init__(self, name, id):
+        super().__init__(name, id)
+        self.medias = OrderedLinkedList()
+    
